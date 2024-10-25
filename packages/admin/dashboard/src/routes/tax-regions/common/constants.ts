2acf511fdff43e0c64508bc0edd803271313f118
fPolic <mainacc.polic@gmail.com>
2024-10-25 14:47:17 +0200

    fix: tax override reference

--- packages/admin/dashboard/src/routes/tax-regions/common/constants.ts
+++ packages/admin/dashboard/src/routes/tax-regions/common/constants.ts
@@ -1,7 +1,7 @@
 export enum TaxRateRuleReferenceType {
-  PRODUCT = "products",
-  PRODUCT_COLLECTION = "product_collections",
-  PRODUCT_TAG = "product_tags",
-  PRODUCT_TYPE = "product_types",
-  CUSTOMER_GROUP = "customer_groups",
+  PRODUCT = "product",
+  PRODUCT_COLLECTION = "product_collection",
+  PRODUCT_TAG = "product_tag",
+  PRODUCT_TYPE = "product_type",
+  CUSTOMER_GROUP = "customer_group",
 }
