6ab89ef63eaa2bd5c85268d061b59bb7a3853bdb
Harminder Virk <virk.officials@gmail.com>
2024-12-10 13:08:50 +0530

    refactor: use correct relationship

--- packages/modules/stock-location/src/models/stock-location.ts
+++ packages/modules/stock-location/src/models/stock-location.ts
@@ -1,20 +1,19 @@
 import { model } from "@medusajs/framework/utils"
 import StockLocationAddress from "./stock-location-address"
 
-const StockLocation = model
-  .define("StockLocation", {
-    id: model.id({ prefix: "sloc" }).primaryKey(),
-    name: model.text().searchable(),
-    metadata: model.json().nullable(),
-    address: model
-      .hasOne(() => StockLocationAddress, {
-        foreignKey: true,
-        mappedBy: undefined,
-      })
-      .nullable(),
-  })
-  .cascades({
-    delete: ["address"],
-  })
+const StockLocation = model.define("StockLocation", {
+  id: model.id({ prefix: "sloc" }).primaryKey(),
+  name: model.text().searchable(),
+  metadata: model.json().nullable(),
+  address: model
+    .belongsTo(() => StockLocationAddress, {
+      // foreignKey: true,
+      mappedBy: "stock_locations",
+    })
+    .nullable(),
+})
+// .cascades({
+//   delete: ["address"],
+// })
 
 export default StockLocation
