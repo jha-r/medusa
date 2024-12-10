ba9528aa6c31803dd09f448ea5b1a1c7e429efc2
Harminder Virk <virk.officials@gmail.com>
2024-12-10 15:49:31 +0530

    refactor: migrate to DML

--- packages/modules/tax/src/models/tax-rate-rule.ts
+++ packages/modules/tax/src/models/tax-rate-rule.ts
@@ -1,117 +1,126 @@
-import { DAL } from "@medusajs/framework/types"
-import {
-  createPsqlIndexStatementHelper,
-  DALUtils,
-  generateEntityId,
-} from "@medusajs/framework/utils"
-import {
-  BeforeCreate,
-  Entity,
-  Filter,
-  ManyToOne,
-  OnInit,
-  OptionalProps,
-  PrimaryKey,
-  Property,
-  Rel,
-} from "@mikro-orm/core"
+import { model } from "@medusajs/framework/utils"
 import TaxRate from "./tax-rate"
 
-const TABLE_NAME = "tax_rate_rule"
+// const TABLE_NAME = "tax_rate_rule"
 
-type OptionalRuleProps = DAL.SoftDeletableModelDateColumns
+// const taxRateIdIndexName = "IDX_tax_rate_rule_tax_rate_id"
+// const taxRateIdIndexStatement = createPsqlIndexStatementHelper({
+//   name: taxRateIdIndexName,
+//   tableName: TABLE_NAME,
+//   columns: "tax_rate_id",
+//   where: "deleted_at IS NULL",
+// })
+// const deletedAtIndexStatement = createPsqlIndexStatementHelper({
+//   tableName: TABLE_NAME,
+//   columns: "deleted_at",
+//   where: "deleted_at IS NOT NULL",
+// })
 
-const taxRateIdIndexName = "IDX_tax_rate_rule_tax_rate_id"
-const taxRateIdIndexStatement = createPsqlIndexStatementHelper({
-  name: taxRateIdIndexName,
-  tableName: TABLE_NAME,
-  columns: "tax_rate_id",
-  where: "deleted_at IS NULL",
-})
-const deletedAtIndexStatement = createPsqlIndexStatementHelper({
-  tableName: TABLE_NAME,
-  columns: "deleted_at",
-  where: "deleted_at IS NOT NULL",
-})
+// const referenceIdIndexName = "IDX_tax_rate_rule_reference_id"
+// const referenceIdIndexStatement = createPsqlIndexStatementHelper({
+//   name: referenceIdIndexName,
+//   tableName: TABLE_NAME,
+//   columns: "reference_id",
+//   where: "deleted_at IS NULL",
+// })
 
-const referenceIdIndexName = "IDX_tax_rate_rule_reference_id"
-const referenceIdIndexStatement = createPsqlIndexStatementHelper({
-  name: referenceIdIndexName,
-  tableName: TABLE_NAME,
-  columns: "reference_id",
-  where: "deleted_at IS NULL",
-})
+// export const uniqueRateReferenceIndexName =
+//   "IDX_tax_rate_rule_unique_rate_reference"
+// const uniqueRateReferenceIndexStatement = createPsqlIndexStatementHelper({
+//   name: uniqueRateReferenceIndexName,
+//   tableName: TABLE_NAME,
+//   columns: ["tax_rate_id", "reference_id"],
+//   unique: true,
+//   where: "deleted_at IS NULL",
+// })
 
-export const uniqueRateReferenceIndexName =
-  "IDX_tax_rate_rule_unique_rate_reference"
-const uniqueRateReferenceIndexStatement = createPsqlIndexStatementHelper({
-  name: uniqueRateReferenceIndexName,
-  tableName: TABLE_NAME,
-  columns: ["tax_rate_id", "reference_id"],
-  unique: true,
-  where: "deleted_at IS NULL",
-})
-
-@Entity({ tableName: TABLE_NAME })
-@Filter(DALUtils.mikroOrmSoftDeletableFilterOptions)
-@uniqueRateReferenceIndexStatement.MikroORMIndex()
-export default class TaxRateRule {
-  [OptionalProps]?: OptionalRuleProps
-
-  @PrimaryKey({ columnType: "text" })
-  id!: string
-
-  @ManyToOne(() => TaxRate, {
-    type: "text",
-    fieldName: "tax_rate_id",
-    mapToPk: true,
-    onDelete: "cascade",
+const TaxRateRule = model
+  .define("TaxRateRule", {
+    id: model.id({ prefix: "txrule" }).primaryKey(),
+    metadata: model.json().nullable(),
+    created_by: model.text().nullable(),
+    tax_rate: model.belongsTo(() => TaxRate, {
+      mappedBy: "rules",
+    }),
+    reference: model.text(),
+    reference_id: model.text(),
   })
-  @taxRateIdIndexStatement.MikroORMIndex()
-  tax_rate_id: string
+  .indexes([
+    {
+      name: "IDX_tax_rate_rule_reference_id",
+      on: ["reference_id"],
+      where: "deleted_at IS NULL",
+    },
+    {
+      name: "IDX_tax_rate_rule_unique_rate_reference",
+      on: ["tax_rate_id", "reference_id"],
+      unique: true,
+      where: "deleted_at IS NULL",
+    },
+  ])
 
-  @Property({ columnType: "text" })
-  @referenceIdIndexStatement.MikroORMIndex()
-  reference_id: string
+export default TaxRateRule
 
-  @Property({ columnType: "text" })
-  reference: string
+// @Entity({ tableName: TABLE_NAME })
+// @Filter(DALUtils.mikroOrmSoftDeletableFilterOptions)
+// @uniqueRateReferenceIndexStatement.MikroORMIndex()
+// export default class TaxRateRule {
+// [OptionalProps]?: OptionalRuleProps
 
-  @ManyToOne(() => TaxRate, { persist: false })
-  tax_rate: Rel<TaxRate>
+// @PrimaryKey({ columnType: "text" })
+// id!: string
 
-  @Property({ columnType: "jsonb", nullable: true })
-  metadata: Record<string, unknown> | null = null
+// @ManyToOne(() => TaxRate, {
+//   type: "text",
+//   fieldName: "tax_rate_id",
+//   mapToPk: true,
+//   onDelete: "cascade",
+// })
+// @taxRateIdIndexStatement.MikroORMIndex()
+// tax_rate_id: string
 
-  @Property({
-    onCreate: () => new Date(),
-    columnType: "timestamptz",
-    defaultRaw: "now()",
-  })
-  created_at: Date
+// @Property({ columnType: "text" })
+// @referenceIdIndexStatement.MikroORMIndex()
+// reference_id: string
 
-  @Property({
-    onCreate: () => new Date(),
-    onUpdate: () => new Date(),
-    columnType: "timestamptz",
-    defaultRaw: "now()",
-  })
-  updated_at: Date
+// @Property({ columnType: "text" })
+// reference: string
 
-  @Property({ columnType: "text", nullable: true })
-  created_by: string | null = null
+// @ManyToOne(() => TaxRate, { persist: false })
+// tax_rate: Rel<TaxRate>
 
-  @deletedAtIndexStatement.MikroORMIndex()
-  @Property({ columnType: "timestamptz", nullable: true })
-  deleted_at: Date | null = null
+// @Property({ columnType: "jsonb", nullable: true })
+// metadata: Record<string, unknown> | null = null
 
-  @BeforeCreate()
-  onCreate() {
-    this.id = generateEntityId(this.id, "txrule")
-  }
+// @Property({
+//   onCreate: () => new Date(),
+//   columnType: "timestamptz",
+//   defaultRaw: "now()",
+// })
+// created_at: Date
 
-  @OnInit()
-  onInit() {
-    this.id = generateEntityId(this.id, "txrule")
-  }
-}
+// @Property({
+//   onCreate: () => new Date(),
+//   onUpdate: () => new Date(),
+//   columnType: "timestamptz",
+//   defaultRaw: "now()",
+// })
+// updated_at: Date
+
+// @Property({ columnType: "text", nullable: true })
+// created_by: string | null = null
+
+// @deletedAtIndexStatement.MikroORMIndex()
+// @Property({ columnType: "timestamptz", nullable: true })
+// deleted_at: Date | null = null
+
+// @BeforeCreate()
+// onCreate() {
+//   this.id = generateEntityId(this.id, "txrule")
+// }
+
+// @OnInit()
+// onInit() {
+//   this.id = generateEntityId(this.id, "txrule")
+// }
+// }
