d20433a0ff150f880f04e8f678365db81aaaa0ef
jha-r <call.4398@gmail.com>
2024-12-13 14:45:20 -0500

    Create medusa-config.ts

---
+++ medusa-config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  projectConfig: {
+    redis_url: REDIS_URL,
+    database_url: DATABASE_URL,
+    database_type: "postgres",
+    store_cors: STORE_CORS,
+    admin_cors: ADMIN_CORS,
+  },
+  plugins,
+};
